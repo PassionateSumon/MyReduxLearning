36165494bf963c72219f1b4acd8b5307746641f2
Sumon Mitra <sumonmitra.21@gmail.com>
2024-06-02 23:24:42 +0530

    complex state management with cartitems and wishlist

--- prac.js
+++ prac.js
@@ -1,45 +1,114 @@
 import { createStore } from "redux";
-import { customCreateStore } from "./myCustomRedux";
-
-const postCount = document.querySelector(".post-count");
-const incBttn = document.querySelector(".btn1");
-const decBttn = document.querySelector(".btn2");
+import { productsList } from "./productsList";
 
 const initialState = {
-  post: 1,
-  name: "Sumon Mitra",
-  age: 22,
+  products: productsList,
+  cartItems: [],
+  wishList: [],
 };
+
+const CART_ADD_ITEMS = "cart/addItem";
+const CART_DELETE_ITEMS = "cart/deleteItem";
+const CART_INC_QUANTITY = "cart/increaseQuant";
+const CART_DEC_QUANTITY = "cart/decreaseQuant";
+
+const WISHLIST_ADD_ITEMS = "wishList/addItem";
+const WISHLIST_REMOVE_ITEMS = "wishList/removeItem";
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case "post/increment":
-      return { ...state, post: state.post + 1 };
-    case "post/decrement":
-      return { ...state, post: state.post - 1 };
-    case "post/incrementBy":
-      return { ...state, post: state.post + action.payload };
+    case CART_ADD_ITEMS:
+      return { ...state, cartItems: [...state.cartItems, action.payload] };
+    case CART_DELETE_ITEMS:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((cb) => {
+          return cb.productId !== action.payload.productId;
+        }),
+      };
+    case CART_INC_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems.map((item) => {
+          return item.productId === action.payload.productId
+            ? { ...item, quantity: item.quantity + 1 }
+            : item;
+        }),
+      };
+    case CART_DEC_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems
+          .map((item) => {
+            if (item.productId === action.payload.productId) {
+              return { ...item, quantity: item.quantity - 1 };
+            }
+            return item;
+          })
+          .filter((items) => items.quantity > 0),
+      };
+
+    case WISHLIST_ADD_ITEMS:
+      return { ...state, wishList: [...state.wishList, action.payload] };
+    case WISHLIST_REMOVE_ITEMS:
+      return {
+        ...state,
+        wishList: state.wishList.filter((cb) => {
+          // console.log(cb.productId);
+          console.log(action.payload.productId);
+          return cb.productId !== action.payload.productId;
+        }),
+      };
   }
   return state;
 }
 
-const store = customCreateStore(reducer, __REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducer, __REDUX_DEVTOOLS_EXTENSION__());
 
-const unsubs = store.subscribe(() => {
-  console.log(store.getState());
-  postCount.innerText = store.getState().post;
+store.dispatch({
+  type: CART_ADD_ITEMS,
+  payload: { productId: 1, quantity: 1 },
+});
+store.dispatch({
+  type: CART_ADD_ITEMS,
+  payload: { productId: 11, quantity: 1 },
+});
+store.dispatch({
+  type: CART_ADD_ITEMS,
+  payload: { productId: 12, quantity: 1 },
+});
+store.dispatch({
+  type: CART_ADD_ITEMS,
+  payload: { productId: 13, quantity: 1 },
+});
+store.dispatch({
+  type: CART_ADD_ITEMS,
+  payload: { productId: 14, quantity: 1 },
+});
+store.dispatch({
+  type: CART_ADD_ITEMS,
+  payload: { productId: 15, quantity: 1 },
 });
 
-postCount.innerText = store.getState().post;
+store.dispatch({ type: CART_DELETE_ITEMS, payload: { productId: 11 } });
+store.dispatch({ type: CART_DELETE_ITEMS, payload: { productId: 15 } });
 
-store.dispatch({ type: "post/increment" });
-store.dispatch({ type: "post/decrement" });
-unsubs()
-store.dispatch({ type: "post/incrementBy", payload: 7 });
+store.dispatch({ type: CART_INC_QUANTITY, payload: { productId: 14 } });
+store.dispatch({ type: CART_INC_QUANTITY, payload: { productId: 14 } });
+store.dispatch({ type: CART_INC_QUANTITY, payload: { productId: 12 } });
+store.dispatch({ type: CART_INC_QUANTITY, payload: { productId: 12 } });
+store.dispatch({ type: CART_INC_QUANTITY, payload: { productId: 1 } });
 
-incBttn.addEventListener("click", () => {
-  store.dispatch({ type: "post/incrementBy", payload: 7 });
-});
-decBttn.addEventListener("click", () => {
-  store.dispatch({ type: "post/decrement" });
-});
+store.dispatch({ type: CART_DEC_QUANTITY, payload: { productId: 1 } });
+store.dispatch({ type: CART_DEC_QUANTITY, payload: { productId: 12 } });
+store.dispatch({ type: CART_DEC_QUANTITY, payload: { productId: 14 } });
+store.dispatch({ type: CART_DEC_QUANTITY, payload: { productId: 14 } });
+store.dispatch({ type: CART_DEC_QUANTITY, payload: { productId: 14 } });
+
+store.dispatch({ type: WISHLIST_ADD_ITEMS, payload: { productId: 1 } });
+store.dispatch({ type: WISHLIST_ADD_ITEMS, payload: { productId: 12 } });
+store.dispatch({ type: WISHLIST_ADD_ITEMS, payload: { productId: 21 } });
+store.dispatch({ type: WISHLIST_ADD_ITEMS, payload: { productId: 14 } });
+
+store.dispatch({ type: WISHLIST_REMOVE_ITEMS, payload: { productId: 12 } });
+store.dispatch({ type: WISHLIST_REMOVE_ITEMS, payload: { productId: 21 } });
